862d817fdd5a45c6060cc99734dccc3f9870e3e5
Fabio Souza <fabiords7@gmail.com>
2021-05-21 11:32:41 -0300

    Projeto Iniciado

---
+++ Course/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+namespace Course
+{
+    class Employee
+    {
+
+        
+
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var x = 10;
+            var y = 20.0;
+            var z = "Jão";
+
+            Console.WriteLine(z);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+        }
+    }
+}
